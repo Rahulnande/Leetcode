0c5df120f6aaccdf995acc8eb5f8593219c5caf6
Rahulnande <nanderahul34@gmail.com>
2026-06-02 11:57:06 +0530

    send to gitub

---
+++ src/practical/Leetcode1_Two_Sum.java
@@ -0,0 +1,39 @@
+package practical;
+
+import java.util.Arrays;
+
+//Example 1:
+//
+//Input: nums = [2,7,11,15], target = 9
+//Output: [0,1]
+//Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+
+
+class Solution{
+	public int[] twosum(int []nums,int target)
+	{
+		for (int i = 0; i < nums.length; i++)
+		{
+			for (int j = i+1; j < nums.length; j++)
+			{
+				if(nums[i]+nums[j]==target)
+				{
+					return new int[]{i,j};
+				}
+			}
+		}
+		return new int[]{};
+	}
+}
+public class Leetcode1_Two_Sum {
+	public static void main(String[] args) {
+		
+		Solution obj=new Solution();
+		int nums[]= {2,7,11,15};
+		int target=9;
+		int result[]=obj.twosum(nums,target);
+		 System.out.println(Arrays.toString(result));
+		
+	}
+
+}
